e4faf436aa8870b385ba22113e34ac78e928cc11
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-22 14:46:47 +0100

    Add initial Package.swift

---
+++ Package.swift
@@ -0,0 +1,49 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-coder-primitives",
+    platforms: [
+        .macOS(.v26),
+        .iOS(.v26),
+        .tvOS(.v26),
+        .watchOS(.v26),
+        .visionOS(.v26),
+    ],
+    products: [
+        .library(
+            name: "Coder Primitives",
+            targets: ["Coder Primitives"]
+        ),
+    ],
+    dependencies: [
+        .package(path: "../swift-serialization-primitives"),
+        .package(path: "../swift-binary-primitives"),
+        .package(path: "../swift-ascii-primitives"),
+        .package(path: "../swift-error-primitives"),
+    ],
+    targets: [
+        .target(
+            name: "Coder Primitives",
+            dependencies: [
+                .product(name: "Serialization Primitives", package: "swift-serialization-primitives"),
+                .product(name: "Binary Primitives", package: "swift-binary-primitives"),
+                .product(name: "ASCII Primitives", package: "swift-ascii-primitives"),
+                .product(name: "Error Primitives", package: "swift-error-primitives"),
+            ]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
+
+for target in package.targets where ![.system, .binary, .plugin, .macro].contains(target.type) {
+    let settings: [SwiftSetting] = [
+        .enableUpcomingFeature("ExistentialAny"),
+        .enableUpcomingFeature("InternalImportsByDefault"),
+        .enableUpcomingFeature("MemberImportVisibility"),
+        .enableExperimentalFeature("Lifetimes"),
+        .strictMemorySafety(),
+    ]
+    target.swiftSettings = (target.swiftSettings ?? []) + settings
+}
